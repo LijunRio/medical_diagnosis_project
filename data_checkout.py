9cbf6d35e6b1cab6556efface311bd5a4785768d
Rio <rioin1998@163.com>
2021-08-09 19:25:02 +0800

    update model path

--- data_checkout.py
+++ data_checkout.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 #         print('None:', image_file)
 
 # img1 = cv2.imread('../data/image/CXR4_IM-2050-2001.png')
-img2 = cv2.imread('./CXR2185_IM-0795-2001.png')
+img2 = cv2.imread('../data/image/CXR2185_IM-0795-2001.png')
 
 # print(img1.shape)
 print(img2.shape)
