df972612e4da2e47f1fadedda20f2c0b3b795fbf
Rio <rioin1998@163.com>
2021-08-09 17:44:51 +0800

    update model path

--- data_checkout.py
+++ data_checkout.py
@@ -5,7 +5,4 @@ from tqdm import tqdm
 image_folder = '../data/image'  # path to folder containing images
 for file in tqdm(os.listdir(image_folder)):
     image_file = os.path.join(image_folder, file)
-    try:
-        cv2.imread(image_file)
-    except:
-        print(image_file)
+    cv2.imread(image_file)
