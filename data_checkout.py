1eaa32a2a3b5484943f2229ed7098773de31129e
Rio <rioin1998@163.com>
2021-08-09 17:48:32 +0800

    update model path

--- data_checkout.py
+++ data_checkout.py
@@ -4,5 +4,8 @@ from tqdm import tqdm
 
 image_folder = '../data/image'  # path to folder containing images
 for file in tqdm(os.listdir(image_folder)):
+
     image_file = os.path.join(image_folder, file)
-    cv2.imread(image_file)
+    img = cv2.imread(image_file)
+    if img is None:
+        print('None:', image_file)
