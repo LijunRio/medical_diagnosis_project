8bbb538bf54e85f695793bcd210be318de305c98
rio <rioin1998@153.com>
2021-08-28 21:16:19 +0800

    train in create_model.py

--- final.py
+++ final.py
@@ -8,11 +8,11 @@ import create_model as cm
 
 st.title("Chest X-ray Report Generator")
 
-st.markdown("<small>by Ashish</small>",unsafe_allow_html=True)
-st.markdown("[<small>Github</small>](https://github.com/ashishthomaschempolil/Medical-Image-Captioning-on-Chest-X"
-            "-rays)          [<small>Towards Data Science</small>]("
-            "https://towardsdatascience.com/medical-image-captioning-on-chest-x-rays-a43561a6871d)",
-unsafe_allow_html=True)
+# st.markdown("<small>by Ashish</small>",unsafe_allow_html=True)
+# st.markdown("[<small>Github</small>](https://github.com/ashishthomaschempolil/Medical-Image-Captioning-on-Chest-X"
+#             "-rays)          [<small>Towards Data Science</small>]("
+#             "https://towardsdatascience.com/medical-image-captioning-on-chest-x-rays-a43561a6871d)",
+# unsafe_allow_html=True)
 st.markdown("\nThis app will generate impression part of an X-ray report.\nYou can upload 2 X-rays that are front "
             "view and side view of chest of the same individual.") 
 st.markdown("The 2nd X-ray is optional.")
