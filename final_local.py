ac913ecf85022fd788c7e6c3d3d416b354de5164
rio <rioin1998@153.com>
2021-08-29 12:28:47 +0800

    final_test

--- final_local.py
+++ final_local.py
@@ -11,6 +11,25 @@ def create_model():
     return model_tokenizer
 
 
+def predict(image_1, image_2, model_tokenizer):
+    image_1 = Image.open(image_1).convert("RGB")  # converting to 3 channels
+    image_1 = np.array(image_1) / 255
+    if image_2 is None:
+        image_2 = image_1
+    else:
+        image_2 = Image.open(image_2).convert("RGB")  # converting to 3 channels
+        image_2 = np.array(image_2) / 255
+    st.image([image_1, image_2], width=300)
+    caption = cm.function1([image_1], [image_2], model_tokenizer)
+    st.markdown(" ### **Impression:**")
+    impression = st.empty()
+    impression.write(caption[0])
+    time_taken = "Time Taken for prediction: %i seconds" % (time.process_time() - start)
+    st.write(time_taken)
+    del image_1, image_2
+
+
+
 def predict_sample(model_tokenizer, folder='./test_images'):
     no_files = len(os.listdir(folder))
     file = np.random.randint(1, no_files)
