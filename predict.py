942789ed3bc251a75a862df8a4141da0806aefc9
rio <rioin1998@153.com>
2021-08-29 16:21:10 +0800

    final_test

--- predict.py
+++ predict.py
@@ -4,9 +4,11 @@ from config import config as args
 from create_model import function1
 import pandas
 
+# read the test image path
 file_name = 'test.pkl'
 test = pd.read_pickle(os.path.join(args.finalPkl_ph, file_name))
 print(test.columns.values.tolist())
 image1_pth = test['image_1'].values.tolist()
 image2_pth = test['image_2'].values.tolist()
-print('image1_path:', image1_pth[0])
+
+function1(image1_pth, image2_pth)
